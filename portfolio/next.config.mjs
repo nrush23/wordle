0a3a4e57a3992892844e8b06a206631326306574
N.R. <nrush@drew.edu>
2025-09-16 12:07:09 -0400

    Production mode path added

--- portfolio/next.config.mjs
+++ portfolio/next.config.mjs
@@ -1,13 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: "export",
-    images: {unoptimized: true},
-    basePath: "/~ncr7500",
-    assetPrefix: "/~ncr7500/",
-    trailingSlash: true,
-    env: {
-        BASE_PATH: "/~ncr7500"
-    }
-};
+export default () => {
+    const isProd = process.env.NODE_ENV === "production";
+    const prefix = isProd ? "/~ncr7500" : "";
 
-export default nextConfig;
+    return {
+        output: "export",
+        trailingSlash: true,
+        images: { unoptimized: true },
+        basePath: prefix,
+        env: {
+            BASE_PATH: prefix,
+        },
+    };
+};
+// const nextConfig = {
+//     output: "export",
+//     images: {unoptimized: true},
+//     basePath: "/~ncr7500",
+//     assetPrefix: "/~ncr7500/",
+//     trailingSlash: true,
+//     env: {
+//         BASE_PATH: "/~ncr7500"
+//     }
+// };
+
+// export default nextConfig;
